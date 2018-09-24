17d9937b2ff53bcac60fb51de41300c0a27fa3ee
Reuben Ayres <ra@sent.com>
2018-09-24 17:22:02 -0500

    pseudocode to clear screen at start of second number input

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 console.log("hey calc");
 
+// this is where arg 1 is sstored
+// MVP: update when oper pressed 
+// TODO later after MVP: update this on each button click 
+// important concept: data the user can see should be directly representative of some data in your app
+let num1; 
+let oper = null;
+
+
 $('.num').on('click', (e) => {
   // console.log(e)
   // console.log(e.currentTarget)
-  const $jqOfBtnClicked = $(e.currentTarget)
 
+  const $jqOfBtnClicked = $(e.currentTarget)
   const numClicked = $jqOfBtnClicked.text()
+
+
+  // if the person just hit an operator 
+
+    // clear the screen 
+    
+
+
 
   // put the number in the display
   if($('#display').text() == "0") {
@@ -18,3 +34,16 @@ $('.num').on('click', (e) => {
   // $('#display').text(whatShouldBeInDispAfterClick)
 
 })
+
+
+// when an operator is clicked
+    // store number from display in num1
+    // store operator -- 
+    // clear display
+
+
+// when eq is clicked
+    // store second number
+    // do the math
+    // store the result  in num1
+    // show num1
